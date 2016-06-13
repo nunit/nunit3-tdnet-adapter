34594873f9dce6212c27afbff60d659aa0d3f769
Jamie Cansdale <jcansdale@gmail.com>
2016-06-13 11:03:59 +0100

    Create initial project structure to get working with CI server. (#1)
    * Create initial project structure to get working wth CI server.
    
    * Cleaned up.

---
+++ src/NUnitTDNet.Adapter.Tests/NUnitTestRunnerTests.cs
@@ -0,0 +1,23 @@
+﻿namespace NUnitTDNet.Adapter.Tests
+{
+    using System;
+    using Fakes;
+    using Examples;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NUnitTestRunnerTests
+    {
+        [TestMethod]
+        public void RunMember_OnePassingTest_PassedCount1()
+        {
+            var testRunner = new NUnitTestRunner();
+            var testListener = new FakeTestListener();
+            var testClass = typeof(OnePassingTest);
+
+            testRunner.RunMember(testListener, testClass.Assembly, testClass);
+
+            Assert.AreEqual(1, testListener.PassedCount, "Check one test passed.");
+        }
+    }
+}
---
+++ src/NUnitTDNet.Adapter/NUnitTestRunner.cs
@@ -0,0 +1,24 @@
+﻿namespace NUnitTDNet.Adapter
+{
+    using System;
+    using System.Reflection;
+    using TestDriven.Framework;
+
+    public class NUnitTestRunner : ITestRunner
+    {
+        public TestRunState RunAssembly(ITestListener testListener, Assembly assembly)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TestRunState RunMember(ITestListener testListener, Assembly assembly, MemberInfo member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TestRunState RunNamespace(ITestListener testListener, Assembly assembly, string ns)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
