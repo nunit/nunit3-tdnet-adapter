7ced9b4fbcafac51230f82411f551a09ecf141c0
Jamie Cansdale <jcansdale+github@gmail.com>
2016-06-13 13:19:55 +0100

    Simplest possible implementation that will execute one test. Let TestDriven.Net know which test runner to use for Examples project.

---
+++ src/NUnitTDNet.Adapter.Examples/NUnitTDNet.Adapter.Examples/CustomTestRunner.cs
@@ -0,0 +1,5 @@
+﻿using NUnitTDNet.Adapter;
+using TestDriven.Framework;
+
+// Tell TestDriven.Net to use this test runner when targeting tests in this assembly.
+[assembly: CustomTestRunner(typeof(NUnitTestRunner))]
---
+++ src/NUnitTDNet.Adapter.Examples/NUnitTDNet.Adapter.Examples/SomeTests.cs
@@ -0,0 +1,19 @@
+﻿namespace NUnitTDNet.Adapter.Examples
+{
+    using NUnit.Framework;
+
+    public class SomeTests
+    {
+        [Test]
+        public static void Pass()
+        {
+            Assert.That(2 + 2, Is.EqualTo(4));
+        }
+
+        [Test]
+        public static void Fail()
+        {
+            Assert.That(2 + 2, Is.EqualTo(5));
+        }
+    }
+}
