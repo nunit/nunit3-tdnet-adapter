9146543b4573f4ba69a6a2dccdec7fb8f2bc8156
Jamie Cansdale <jcansdale@gmail.com>
2016-06-23 17:06:51 +0100

    Added WiX installer definition file for TestDriven.Net that mirrors NuGet package.

--- vars.bat
+++ vars.bat
@@ -3,4 +3,5 @@
 @echo To build use 'msbuild'
 
 set path=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319;%path%
-set path=%~dp0nuget;%path%
+set path=%~dp0tools\NuGet;%path%
+set path=%~dp0tools\WiX;%path%
