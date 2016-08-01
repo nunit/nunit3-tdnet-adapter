1853c5eba47ba09b5ba6154526219f1913710a11
Jamie Cansdale <jcansdale@gmail.com>
2016-08-01 11:06:24 +0100

    Tests for issue #7 (can't fall back to running "Ad hoc" test).

---
+++ src/NUnitTDNet.Adapter.Examples/Expected/AdHocTests.cs
@@ -0,0 +1,22 @@
+﻿namespace NUnitTDNet.Adapter.Examples.Expected
+{
+    using TestDriven.Framework;
+
+    [ExpectTestRun(TestRunState.NoTests)]
+    public class AdHocTests
+    {
+        [ExpectTestRun(TestRunState.NoTests)]
+        public void MethodNoTestAttribute()
+        {
+        }
+
+        [ExpectTestRun(TestRunState.NoTests)]
+        public string TargetProperty
+        {
+            get; set;
+        }
+
+        [ExpectTestRun(TestRunState.NoTests)]
+        public string TargetField;
+    }
+}
